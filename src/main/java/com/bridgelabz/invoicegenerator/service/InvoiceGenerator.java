042ccfcf6889807fd2ea58ce1e454bc407be460e
“Shubham” <shubhampattar2@gmail.com>
2020-07-11 10:30:17 +0530

    ADDED Test Case And Code To Return Minimum Fare If Total Fare Is Less Than Minimum Fare

--- src/main/java/com/bridgelabz/invoicegenerator/service/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/invoicegenerator/service/InvoiceGenerator.java
@@ -3,8 +3,10 @@ package com.bridgelabz.invoicegenerator.service;
 public class InvoiceGenerator {
     private static final double COST_PER_KM = 10.0;
     private static final int COST_PER_MIN = 1;
+    private static final double MINIMUM_FARE = 5.0;
 
     public double calculateFare(double distance, int time) {
-        return distance * COST_PER_KM + time * COST_PER_MIN;
+        double totalFare = distance * COST_PER_KM + time * COST_PER_MIN;
+        return Math.max(totalFare, MINIMUM_FARE);
     }
 }
--- src/test/java/com/bridgelabz/invoicegenerator/InvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/invoicegenerator/InvoiceGeneratorTest.java
@@ -15,4 +15,14 @@ public class InvoiceGeneratorTest {
         Assert.assertEquals(37, fare, 0.0);
     }
 
+    @Test
+    public void givenLessDistanceAndTime_ShouldReturnMinimumFare()
+    {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.01;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5, fare, 0.0);
+    }
+
 }
