6af41dcf39da435c2750e39754d757d26f8a535c
“Shubham” <shubhampattar2@gmail.com>
2020-07-12 22:26:12 +0530

    REFACTORED Code To Achieve Open Close Principle Using Enum And Optimized Code.

--- src/main/java/com/bridgelabz/invoicegenerator/service/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/invoicegenerator/service/InvoiceGenerator.java
@@ -2,14 +2,12 @@ package com.bridgelabz.invoicegenerator.service;
 
 import com.bridgelabz.invoicegenerator.exception.InvoiceGeneratorException;
 import com.bridgelabz.invoicegenerator.model.InvoiceSummary;
-import com.bridgelabz.invoicegenerator.utility.Ride;
+import com.bridgelabz.invoicegenerator.model.Ride;
 import com.bridgelabz.invoicegenerator.utility.RideRepository;
 
 public class InvoiceGenerator
 {
-    private static double COST_PER_KM;
-    private static int COST_PER_MINUTE;
-    private static double MINIMUM_FARE;
+
     RideRepository rideRepository;
 
     public InvoiceGenerator()
@@ -25,35 +23,15 @@ public class InvoiceGenerator
     public InvoiceSummary calculateFare(Ride... rides)
     {
         double totalFare = 0;
+        double rideFare;
         for (Ride ride : rides)
         {
-            checkRideType(ride.rideType);
-            totalFare += ride.distance * COST_PER_KM + ride.time * COST_PER_MINUTE;
+            rideFare = ride.distance * ride.rideType.COST_PER_KM + ride.time * ride.rideType.COST_PER_MINUTE;
+            if (rideFare < ride.rideType.MINIMUM_FARE)
+                rideFare = ride.rideType.MINIMUM_FARE;
+            totalFare += rideFare;
         }
-        if (totalFare < MINIMUM_FARE)
-            return new InvoiceSummary(rides.length, MINIMUM_FARE);
         return new InvoiceSummary(rides.length, totalFare);
-    }
-
-    /**
-     * METHOD TO CHECK THE TYPE OF RIDE AND FIX THE RATE
-     * @param rideType provides ride types
-     */
-    private void checkRideType(String rideType)
-    {
-        switch (rideType)
-        {
-            case "PREMIUM":
-                COST_PER_KM = 15;
-                COST_PER_MINUTE = 2;
-                MINIMUM_FARE = 20;
-                break;
-            case "NORMAL":
-                COST_PER_KM = 10;
-                COST_PER_MINUTE = 1;
-                MINIMUM_FARE = 5;
-                break;
-        }
     }
 
     /**
--- src/test/java/com/bridgelabz/invoicegenerator/InvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/invoicegenerator/InvoiceGeneratorTest.java
@@ -1,16 +1,17 @@
 package com.bridgelabz.invoicegenerator;
 
 import com.bridgelabz.invoicegenerator.exception.InvoiceGeneratorException;
-import com.bridgelabz.invoicegenerator.model.InvoiceSummary;
 import com.bridgelabz.invoicegenerator.service.InvoiceGenerator;
-import com.bridgelabz.invoicegenerator.utility.Ride;
+import com.bridgelabz.invoicegenerator.model.InvoiceSummary;
+import com.bridgelabz.invoicegenerator.model.Ride;
+import com.bridgelabz.invoicegenerator.utility.RideType;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-public class InvoiceGeneratorTest {
-
+public class InvoiceGeneratorTest
+{
     InvoiceGenerator invoiceGenerator;
 
     @Before
@@ -24,7 +25,7 @@ public class InvoiceGeneratorTest {
     {
         double distance = 3.0;
         int time = 7;
-        InvoiceSummary invoiceSummary = invoiceGenerator.calculateFare(new Ride(distance, time, "NORMAL"));
+        InvoiceSummary invoiceSummary = invoiceGenerator.calculateFare(new Ride(distance, time, RideType.NORMAL));
         InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(1,37);
         Assert.assertEquals(expectedInvoiceSummary, invoiceSummary);
     }
@@ -34,7 +35,7 @@ public class InvoiceGeneratorTest {
     {
         double distance = 0.01;
         int time = 1;
-        InvoiceSummary invoiceSummary = invoiceGenerator.calculateFare(new Ride(distance, time, "NORMAL"));
+        InvoiceSummary invoiceSummary = invoiceGenerator.calculateFare(new Ride(distance, time, RideType.NORMAL));
         InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(1,5);
         Assert.assertEquals(expectedInvoiceSummary, invoiceSummary);
     }
@@ -42,9 +43,9 @@ public class InvoiceGeneratorTest {
     @Test
     public void givenMultipleRides_ShouldReturnTotalFare()
     {
-        Ride[] rides = {new Ride(3.0, 7, "NORMAL"), new Ride(0.01, 1, "NORMAL")};
+        Ride[] rides = {new Ride(3.0, 7, RideType.NORMAL), new Ride(0.01, 1, RideType.NORMAL)};
         InvoiceSummary invoiceSummary = invoiceGenerator.calculateFare(rides);
-        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(rides.length,38.1);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(rides.length,42);
         Assert.assertEquals(expectedInvoiceSummary, invoiceSummary);
     }
 
@@ -52,10 +53,9 @@ public class InvoiceGeneratorTest {
     public void givenUserId_ShouldReturnInvoiceSummary() throws InvoiceGeneratorException
     {
         String[] userId = {"user1", "user2", "user3"};
-        Ride[][] rides =
-                {{new Ride(5.0, 12, "NORMAL"), new Ride(2.5, 6, "NORMAL")},
-                        {new Ride(3.0, 5, "NORMAL"), new Ride(0.01, 1, "NORMAL")},
-                        {new Ride(10.0, 15, "NORMAL"), new Ride(2, 30, "NORMAL")}};
+        Ride[][] rides = {{new Ride(5.0, 12, RideType.NORMAL), new Ride(2.5, 6, RideType.NORMAL)},
+                {new Ride(3.0, 5, RideType.NORMAL), new Ride(0.01, 1, RideType.NORMAL)},
+                {new Ride(10.0, 15, RideType.NORMAL), new Ride(2, 30, RideType.NORMAL)}};
         invoiceGenerator.addRideToRepository(userId, rides);
         InvoiceSummary summary = invoiceGenerator.invoiceForUser(userId[2]);
         InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(rides[2].length, 165.0);
@@ -68,10 +68,9 @@ public class InvoiceGeneratorTest {
         try
         {
             String[] userId = {"user1", "user1", "user3"};
-            Ride[][] rides =
-                    {{new Ride(5.0, 12, "NORMAL"), new Ride(2.5, 6, "NORMAL")},
-                            {new Ride(3.0, 5, "NORMAL"), new Ride(0.01, 1, "NORMAL")},
-                            {new Ride(10.0, 15, "NORMAL"), new Ride(2, 30, "NORMAL")}};
+            Ride[][] rides = {{new Ride(5.0, 12, RideType.NORMAL), new Ride(2.5, 6, RideType.NORMAL)},
+                    {new Ride(3.0, 5, RideType.NORMAL), new Ride(0.01, 1, RideType.NORMAL)},
+                    {new Ride(10.0, 15, RideType.NORMAL), new Ride(2, 30, RideType.NORMAL)}};
             ExpectedException exceptionRule = ExpectedException.none();
             exceptionRule.expect(InvoiceGeneratorException.class);
             invoiceGenerator.addRideToRepository(userId, rides);
@@ -86,13 +85,12 @@ public class InvoiceGeneratorTest {
     public void givenPremiumAndNormalRideForUserId_ShouldReturnInvoiceSummary() throws InvoiceGeneratorException
     {
         String[] userId = {"user1", "user2", "user3"};
-        Ride[][] rides =
-                {{new Ride(5.0, 12, "PREMIUM"), new Ride(2.5, 6, "NORMAL")},
-                        {new Ride(3.0, 5, "PREMIUM"), new Ride(0.01, 1, "PREMIUM")},
-                        {new Ride(10.0, 15, "NORMAL"), new Ride(2, 30, "PREMIUM")}};
+        Ride[][] rides = {{new Ride(5.0, 12, RideType.PREMIUM), new Ride(2.5, 6, RideType.NORMAL)},
+                {new Ride(3.0, 5, RideType.PREMIUM), new Ride(0.01, 1, RideType.PREMIUM)},
+                {new Ride(10.0, 15, RideType.NORMAL), new Ride(2, 30, RideType.PREMIUM)}};
         invoiceGenerator.addRideToRepository(userId, rides);
-        InvoiceSummary summary = invoiceGenerator.invoiceForUser(userId[2]);
-        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(rides[2].length, 205.0);
+        InvoiceSummary summary = invoiceGenerator.invoiceForUser(userId[1]);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(rides[1].length, 75.0);
         Assert.assertEquals(expectedInvoiceSummary, summary);
     }
 }
